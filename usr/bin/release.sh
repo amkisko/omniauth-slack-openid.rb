99edaac51740222de3b98f37d448a92c29f5b448
Andrei Makarov <andrei@kiskolabs.com>
2023-08-28 19:11:42 +0300

    Fix release script

--- usr/bin/release.sh
+++ usr/bin/release.sh
@@ -10,7 +10,7 @@ e() {
 }
 
 GEM_NAME="omniauth-slack-openid"
-VERSION=$(grep -e 's.version.*' $GEM_NAME.gemspec | cut -d '"' -f 2)
+VERSION=$(grep -e 'VERSION = .*' lib/omniauth/slack_openid/version.rb | sed -e "s/.*'\(.*\)'.*/\1/")
 GEM_FILE="$GEM_NAME-$VERSION.gem"
 
 e "gem build $GEM_NAME.gemspec"
